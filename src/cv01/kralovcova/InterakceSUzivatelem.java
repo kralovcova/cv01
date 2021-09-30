f4f7f15101eaaf2b526f51d484275187b197c492
kralovcova <jirina.kralovcova@tul.cz>
2021-09-30 16:44:12 +0200

    StdIO
    Interaktivní komunikace s uzivatelem
    Standardni vstup filtrovany instanci tridy Scanner
    Vypis na standardni vystup
    Lokalizace Scanner a metody format

--- src/cv01/kralovcova/InterakceSUzivatelem.java
+++ src/cv01/kralovcova/InterakceSUzivatelem.java
@@ -5,6 +5,8 @@
 
 package cv01.kralovcova;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jirina
@@ -15,7 +17,37 @@ public class InterakceSUzivatelem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nacteni hodnot od uzivatele");
         
+        System.out.println("");
+        System.out.print("Zadej cele cislo: ");
+        int celeCislo = sc.nextInt(); 
+        sc.nextLine();
+        System.out.print("Bylo zadano cislo: ");
+        System.out.println(celeCislo);
+        
+        System.out.println("");
+        System.out.print("Zadej realne cislo: ");
+        float realneCislo = sc.nextFloat();
+        sc.nextLine();
+        System.out.format("Bylo zadano cislo: %1.3f%n", realneCislo);
+        
+        System.out.println("");
+        System.out.print("Zadej slovo: ");
+        String slovo = sc.next();
+        sc.nextLine();
+        System.out.println("Bylo nacteno: " + slovo);
+        
+        System.out.println("");
+        System.out.println("Nactu delsi text: ");
+        String veta = sc.nextLine();
+        System.out.println("Zadana veta: " );
+        System.out.println("--" + veta + "--");
+        System.out.println("");
+        
+        System.out.println("To je vse!");
+        System.out.println("Koncim!");
     }
 
 }
