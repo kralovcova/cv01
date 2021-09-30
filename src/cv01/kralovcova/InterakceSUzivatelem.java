7f0f9e957f060dd980e2b238f4560ebd23ccdc26
kralovcova <jirina.kralovcova@tul.cz>
2021-09-30 16:34:14 +0200

    StdIO
    Interaktivní komunikace s uzivatelem
    Standardni vstup filtrovany instanci tridy Scanner
    Vypis na standardni vystup
    Lokalizace Scanner a metody format

---
+++ src/cv01/kralovcova/InterakceSUzivatelem.java
@@ -0,0 +1,21 @@
+/*
+ * Licence info
+ * This code is part of free software
+ */
+
+package cv01.kralovcova;
+
+/**
+ *
+ * @author Jirina
+ */
+public class InterakceSUzivatelem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
