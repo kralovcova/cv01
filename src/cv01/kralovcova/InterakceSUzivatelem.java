4353c08bec924aa10e0efa86ee48b945f92d5564
kralovcova <jirina.kralovcova@tul.cz>
2021-09-30 16:58:14 +0200

    StdIO
    Interaktivní komunikace s uzivatelem
    Standardni vstup filtrovany instanci tridy Scanner
    Vypis na standardni vystup
    Lokalizace Scanner a metody format

--- src/cv01/kralovcova/InterakceSUzivatelem.java
+++ src/cv01/kralovcova/InterakceSUzivatelem.java
@@ -5,6 +5,7 @@
 
 package cv01.kralovcova;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -28,10 +29,11 @@ public class InterakceSUzivatelem {
         System.out.println(celeCislo);
         
         System.out.println("");
+        sc.useLocale(Locale.US);
         System.out.print("Zadej realne cislo: ");
         float realneCislo = sc.nextFloat();
         sc.nextLine();
-        System.out.format("Bylo zadano cislo: %1.3f%n", realneCislo);
+        System.out.format(Locale.US, "Bylo zadano cislo: %1.3f%n", realneCislo);
         
         System.out.println("");
         System.out.print("Zadej slovo: ");
