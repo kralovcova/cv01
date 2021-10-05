3b3a0cc635354f6754560c990090a84b21dde0e5
jirina.kralovcova <jirina.kralovcova@TK6-021-mti.TUL>
2021-10-05 17:08:52 +0200

    Kralovcova - vypisy uzivateli

---
+++ src/cv01_git_pokusy/Cv01_git_pokusy.java
@@ -0,0 +1,16 @@
+package cv01_git_pokusy;
+
+/**
+ *
+ * @author jirina.kralovcova
+ */
+public class Cv01_git_pokusy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Poslu to na github ...");
+    }
+    
+}
