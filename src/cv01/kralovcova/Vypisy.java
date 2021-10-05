94655039554af5cc25c40468bcc928282b9e858c
kralovcova <jirina.kralovcova@tul.cz>
2021-10-05 15:41:29 +0200

    StdIO
    Dalsi vypisy uzivateli

---
+++ src/cv01/kralovcova/Vypisy.java
@@ -0,0 +1,22 @@
+/*
+ * Licence info
+ * This code is part of free software
+ */
+
+package cv01.kralovcova;
+
+/**
+ *
+ * @author Jirina
+ */
+public class Vypisy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Text");
+        System.out.println("Cislo " + 5);
+    }
+
+}
